3f153d4ea4f706587d35c55ae451d74fbd04a8d9
Dhiago <dhiagorivera@gmail.com>
2016-10-28 18:09:30 -0300

    prueba

---
+++ Obligatorio/Image.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PicfilLib;
+
+namespace Obligatorio
+{
+    class Image : IPicture
+    {
+        private Bitmap image;
+        public Image(Bitmap img)
+        {
+            image = img;
+        }
+
+        public int Height
+        {
+            get
+            {
+                return image.Height;
+            }
+        }
+
+        public int Width
+        {
+            get
+            {
+                return image.Width;
+            }
+        }
+
+        public IPicture Clone()
+        {
+            Image imageClone = new Image(image);
+            return imageClone;
+        }
+
+        public Color GetColor(int x, int y)
+        {
+            Color color = image.GetPixel(x,y);
+            return color;
+        }
+
+        public void Resize(int width, int height)
+        {
+            image = new Bitmap(image, width, height); 
+        }
+
+        public void SetColor(int x, int y, Color color)
+        {
+            image.SetPixel(x, y, Color.FromArgb(color.A, color.R, color.G, color.B));
+        }
+    }
+}
